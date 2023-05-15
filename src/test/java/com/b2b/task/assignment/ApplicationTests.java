8888f084626653eeeb63b42fa6e612220ef13a7c
Siva Mokamatam <raja_mokamatam@optum.com>
2023-05-15 18:37:52 +0530

    first commit

---
+++ src/test/java/com/b2b/task/assignment/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b2b.task.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
